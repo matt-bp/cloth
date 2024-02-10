4164a190f20f23a6779835b38dfc9e33d72b66be
Matt Bishop <mattsbishop1@gmail.com>
2024-02-09 20:37:25 -0700

    Added fluent assertions to refactor code, and adding new case.

--- Runtime/Cloth/Provider/DirectedEdgeProvider.cs
+++ Runtime/Cloth/Provider/DirectedEdgeProvider.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 
 namespace Cloth.Provider
 {
-    public class DirectedEdgeProvider
+    public static class DirectedEdgeProvider
     {
         public static (DirectedEdge, DirectedEdge) GetEdgesFromTriangle((int, int, int) triangle, Vector3 v1, Vector3 v2, Vector3 v3)
         {
@@ -16,20 +16,19 @@ namespace Cloth.Provider
             var edge2Mag = edge2.magnitude;
             var edge3Mag = edge3.magnitude;
             
-            // if (edge1Mag > edge2Mag && edge1Mag > edge3Mag)
+            if (edge1Mag > edge2Mag && edge1Mag > edge3Mag)
             {
                 // v3 is in the middle
-                return (
-                    new DirectedEdge { Start = triangle.Item2, End = triangle.Item3, Edge = edge2 },
+                return (new DirectedEdge { Start = triangle.Item2, End = triangle.Item3, Edge = edge2 },
                     new DirectedEdge { Start = triangle.Item3, End = triangle.Item1, Edge = edge3 });
             }
             // else if (edge2 > edge1 && edge2 > edge3)
-            // {
-            //     pairs.Add(new SpringPair
-            //         { FirstIndex = triangle.Item3, SecondIndex = triangle.Item1, RestLength = edge3 });
-            //     pairs.Add(new SpringPair
-            //         { FirstIndex = triangle.Item1, SecondIndex = triangle.Item2, RestLength = edge1 });
-            // }
+            else
+            {
+                // v1 is in the middle
+                return (new DirectedEdge { Start = triangle.Item3, End = triangle.Item1, Edge = edge3 },
+                    new DirectedEdge { Start = triangle.Item1, End = triangle.Item2, Edge = edge1 });
+            }
             // else
             // {
             //     pairs.Add(new SpringPair
